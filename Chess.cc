f3a17cfdec689c518ad14cf3a771bf1d9db5c317
Calvinbullock <calvinnbullock@gmail.com>
2022-08-19 17:33:47 -0700

    removed a few wxtra comments

--- Chess.cc
+++ Chess.cc
@@ -22,9 +22,7 @@
 
 // GOLABLE VARIABLES / MEATHODS
 using namespace std;
-void TurnInput(); // Two functions call each other so this keeps the one in scope of the other
-bool DEBUG_MODE;
-string ERROR_MSG = "None";
+string ERROR_MSG = "";
 int TURN_NUM = 0;
 
 // parse the points that the player inputs.
@@ -432,67 +430,6 @@ void BoardReset()
 }
 
 // Prints out the board and all of it's peice placement
-void PrintBoard()
-{
-  std::cout << "Errors: " << ERROR_MSG << endl;
-  std::cout << "Turn: " << TURN_NUM << endl;
-  if (TURN_NUM % 2 == 0)
-  {
-    std::cout << "Purple/White's Turn" << endl;
-  }
-  else
-  {
-    std::cout << "Blue/Black's Turn" << endl;
-  }
-
-  ERROR_MSG = "None";
-
-  std::cout << endl
-            << "  A   B   C   D   E   F   G   H  " << endl;
-  for (int i = 0; i <= 32; i++)
-  {
-    std::cout << "-";
-  }
-
-  // prints out table with inner and outer loops
-  // the loop below is for the rows
-  for (int y = 0; y < 8; y++)
-  {
-    std::cout << endl;
-    // The loop below is for the columns
-    for (int x = 0; x < 8; x++)
-    {
-      if (Board[x][y] == nullptr)
-      {
-        std::cout << "|   ";
-      }
-      else if (Board[x][y]->color == "white")
-      {
-        std::cout << "| "
-                  << "\033[35;1;4m" << Board[x][y]->letter << "\033[0m"
-                  << " ";
-      }
-      else
-      {
-        std::cout << "| "
-                  << "\033[34;1;4m" << Board[x][y]->letter << "\033[0m"
-                  << " ";
-      }
-    }
-    std::cout << "| " << y + 1;
-    std::cout << endl;
-
-    for (int j = 0; j <= 32; j++)
-    {
-      std::cout << "-";
-    }
-  }
-  std::cout << endl;
-  std::cout << "  A   B   C   D   E   F   G   H  " << endl;
-  std::cout << endl;
-}
-
-// Prints out the board and all of it's peice placement
 void PrintBoard_ncurse()
 {
 
@@ -506,13 +443,18 @@ void PrintBoard_ncurse()
   // right now the curses window prints but will move on before the user can see it
   // that needs to be fixed
 
+  // TODO move?
   initscr(); /* Start curses mode */
+
+  clear();
   refresh(); /* Print it on to the real screen */
+
+  // TODO MOVE?
   start_color();
   init_pair(1, COLOR_BLUE, COLOR_BLACK);
   init_pair(2, COLOR_WHITE, COLOR_BLACK);
+  init_pair(3, COLOR_RED, COLOR_BLACK);
 
-  printw("Errors: %s\n", ERROR_MSG.c_str());
   printw("Turn: %s\n", std::to_string(TURN_NUM).c_str());
 
   if (TURN_NUM % 2 == 0)
@@ -524,7 +466,6 @@ void PrintBoard_ncurse()
     printw("Blue/Black's Turn\n");
   }
 
-  ERROR_MSG = "None";
   attron(COLOR_PAIR(2));
 
   printw("\n");
@@ -572,12 +513,13 @@ void PrintBoard_ncurse()
   printw("  A   B   C   D   E   F   G   H  \n");
   printw("\n");
 
-  // TODO this needs to be set better to wait for input
-  // TODO this needs to be set better to wait for input
-  // TODO this needs to be set better to wait for input
-  char *nameTemp = new char[160];
-  printw(nameTemp);
-  getstr(nameTemp);
+  if (!ERROR_MSG.empty())
+  {
+    attron(COLOR_PAIR(3));
+    printw("Errors: %s\n", ERROR_MSG.c_str());
+    attroff(COLOR_PAIR(3));
+  }
+  ERROR_MSG = "None";
 
   attroff(COLOR_PAIR(2));
   endwin(); /* End curses mode */
@@ -594,16 +536,20 @@ void Movement(Position start, Position end)
       Board[start.x][start.y] = nullptr;
       TURN_NUM++;
     }
+    else
+    {
+      ERROR_MSG = "Invalid Move.";
+    }
   }
   else
   {
     ERROR_MSG = "Your start and end coordinates are identicle plaese enter diffrent ones.";
-    TurnInput_Ncurse();
   }
   return;
 }
 
 // Takes input from the payer and parses it for other methods
+void TurnInput_Ncurse()
 {
   Position start, end;
   // **TODO** make sure to check only 2 chars get entered per "cin" otherwise bad things happen
@@ -649,28 +595,19 @@ void Movement(Position start, Position end)
   // This will stop people from entering a nullptr as the start postion.
   if (Board[start.x][start.y] == nullptr)
   {
-    ERROR_MSG = "Starting point has no piece";
-    TurnInput_Ncurse();
+    ERROR_MSG = "Starting point has no piece.";
+    return;
   }
-  else
+
+  // Checks if its the turn of the colour moveing.
+  if ((TURN_NUM % 2 == 0 && Board[start.x][start.y]->color == "black") ||
+      (TURN_NUM % 2 == 1 && Board[start.x][start.y]->color == "white"))
   {
-    // Checks if its the turn of the colour moveing.
-    if (TURN_NUM % 2 == 0 && Board[start.x][start.y]->color == "white")
-    {
-      Movement(start, end);
-      // IS_WHITE_TURN = false;
-    }
-    else if (TURN_NUM % 2 == 1 && Board[start.x][start.y]->color == "black")
-    {
-      Movement(start, end);
-      // IS_WHITE_TURN = true;
-    }
-    else
-    {
-      ERROR_MSG = "Not your turn";
-      TurnInput_Ncurse();
-    }
+    ERROR_MSG = "Not your turn";
+    return;
   }
+
+  Movement(start, end);
 }
 
 // Returns true and prints games over when black or white king dies
@@ -729,6 +666,7 @@ void GameStart()
   while (winner == 0)
   {
     PrintBoard_ncurse();
+    TurnInput_Ncurse();
     winner = IsKingOnBoard();
   }
 }
@@ -745,11 +683,9 @@ int main()
 int main()
 {
   BoardReset();
-
   Position test1 = Position("a1");
   // Position test1 = Position("a3");
   // pawn move check
-
   if (Board[0][1] != nullptr && Board[0][1]->ValidMove(test1, test1))
   {
     std::cout << "Pass" << endl;
@@ -760,7 +696,6 @@ int main()
     std::cout << "Fail" << endl;
     std::cout << ERROR_MSG << endl;
   }
-
   if (Board[0][0] == nullptr)
   {
     std::cout << "Yeah!!!" << endl;
